fa2e64a8656c6f5eb27993acb622cecdd204cf9e
Mario Salazar <mariosalazar.ms.10@gmail.com>
2026-01-03 08:15:08 -0500

    feat: update current reading endpoint and persistence layer

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,18 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(environments.NODE_ENV === 'production' ? 3007 : 4007);
+  //await app.listen(environments.NODE_ENV === 'production' ? 3007 : 4007);
   app.use(morgan('dev'));
 
+  const postgresqlService: DatabaseServicePostgreSQL =
+    new DatabaseServicePostgreSQL();
 
-  const postgresqlService: DatabaseServicePostgreSQL = new DatabaseServicePostgreSQL();
-
-  logger.log(await postgresqlService.connect())
+  logger.log(await postgresqlService.connect());
+  /*
   logger.log(
     `🚀🎉 The Readings microservice is running on: http://localhost:${environments.NODE_ENV === 'production' ? 3007 : 4007}✅`,
   );
+  */
 
   const microservice = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.KAFKA,
@@ -37,6 +39,6 @@ async function bootstrap() {
   });
 
   await microservice.listen();
-  logger.log(`🚀🎉 The Readings - microservice is listening to KAFKA...✅`);
+  logger.log(`Nest application successfully started`);
 }
 bootstrap();
